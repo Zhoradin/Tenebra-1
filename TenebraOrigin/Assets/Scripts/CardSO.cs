83c5796cf60f4cad5ca9600b92cb7c762a631ca0
Zhoradin <necipkocak13@gmail.com>
2024-05-24 17:35:22 +0300

    Tenebra_2

---
+++ TenebraOrigin/Assets/Scripts/CardSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card", order = 1)]
+public class CardSO : ScriptableObject
+{
+    public string cardName;
+    [TextArea]
+    public string actionDescription, cardLore;
+
+    public int currentHealth, attackPower, manaCost;
+
+    public Sprite characterSprite, bgSprite;
+}
---
+++ TenebraOrigin/Assets/Scripts/UIController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public TMP_Text playerManaText, playerHealthText, enemyManaText, enemyHealthText;
+
+    public GameObject manaWarning;
+    public float manaWarningTime;
+    private float manaWarningCounter;
+
+    public GameObject drawCardButton, endTurnButton;
+
+    public UIDamageIndicator playerDamage, enemyDamage;
+
+    public GameObject battleEndScreen;
+    public TMP_Text battleResultText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(manaWarningCounter > 0)
+        {
+            manaWarningCounter -= Time.deltaTime;
+
+            if(manaWarningCounter <= 0)
+            {
+                manaWarning.SetActive(false);
+            }
+        }
+    }
+
+    public void SetPlayerManaText(int manaAmount)
+    {
+        playerManaText.text = "Mana: " + manaAmount;
+    }
+
+    public void SetPlayerHealthText(int healthAmount)
+    {
+        playerHealthText.text = "Player Health: " + healthAmount;
+    }
+
+    public void SetEnemyManaText(int manaAmount)
+    {
+        enemyManaText.text = "Mana: " + manaAmount;
+    }
+
+    public void SetEnemyHealthText(int healthAmount)
+    {
+        enemyHealthText.text = "Enemy Health: " + healthAmount;
+    }
+
+    public void ShowManaWarning()
+    {
+        manaWarning.SetActive(true);
+        manaWarningCounter = manaWarningTime;
+    }
+
+    public void DrawCard()
+    {
+        DeckController.instance.DrawCardForMana();
+    }
+
+    public void EndPlayerTurn()
+    {
+        BattleController.instance.EndPlayerTurn();
+    }
+
+    public void MainMenu()
+    {
+
+    }
+
+    public void RestartLevel()
+    {
+
+    }
+
+    public void ChooseNewBattle()
+    {
+
+    }
+}
